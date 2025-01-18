a7d7953029eee0a806741281a2ff5d737f08b25e
takagino <178282378+highwoodfield@users.noreply.github.com>
2025-01-18 12:04:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Obsidian Daily Notes Advanced
+
+これはObsidianのためのプラグインです。
+リボンの `Create New Daily Note` ボタンを押すことで、新しいノートを `Daily/YYYY-MM-DD/HH-mm.md`  に作成します。
+日記を複数のファイルとして、日付ごとに分けて整理できることが特徴です。
---
+++ main.ts
@@ -0,0 +1,81 @@
+import { moment, App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface DNASettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: DNASettings = {
+	mySetting: 'default'
+}
+
+const TARGET_FOLDER = "Daily"
+const DATE_FORMAT = "YYYY-MM-DD"
+const TIME_FORMAT = "HH-mm"
+
+export default class DNAPlugin extends Plugin {
+	settings: DNASettings;
+
+	async onload() {
+		// await this.loadSettings();
+
+		this.addRibbonIcon('notebook-pen', 'Create New Daily Note', (evt: MouseEvent) => {
+			this.createDailyNote()
+		});
+
+		// this.addSettingTab(new DNASettingTab(this.app, this));
+	}
+
+	async createDailyNote() {
+		const now = moment()
+		await this.createFolderIfNeeded(TARGET_FOLDER)
+		const pathByDate = TARGET_FOLDER + "/" + now.format(DATE_FORMAT)
+		await this.createFolderIfNeeded(pathByDate)
+		const file = await this.app.vault.create(pathByDate + "/" + now.format(TIME_FORMAT) + ".md", "")
+		const leaf = this.app.workspace.getLeaf(false)
+		leaf.openFile(file)
+	}
+
+	async createFolderIfNeeded(path: string) {
+		const folder = this.app.vault.getFolderByPath(path)
+		if (folder !== null) return
+		await this.app.vault.createFolder(path)
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		// await this.saveData(this.settings);
+	}
+}
+
+class DNASettingTab extends PluginSettingTab {
+	plugin: DNAPlugin;
+
+	constructor(app: App, plugin: DNAPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
